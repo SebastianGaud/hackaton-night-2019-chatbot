4138a9a6e3e1e8cc84d33e5fbbd7841ef707a592
SebastianGaud <sebastiano.gaudeano@live.com>
2019-09-20 19:59:21 +0200

    add config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+export function getBaseUrl() {
+    return config.base_url;
+}
+
+const providers = [{ provide: "BASE_URL", useFactory: getBaseUrl, deps: [] }];
+
+platformBrowserDynamic(providers)
+    .bootstrapModule(AppModule)
+    .catch(err => console.error(err));
